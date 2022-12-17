94b87882f4383754e211f385e840ce0cd0bebceb
dev001 <dev001abc@gmail.com>
2022-12-17 12:22:28 +0900

    add insert function

--- src/main/java/com/example/testproject/controller/IndexController.java
+++ src/main/java/com/example/testproject/controller/IndexController.java
@@ -3,9 +3,12 @@ package com.example.testproject.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.example.testproject.dto.ContentsForm;
 import com.example.testproject.service.TestService;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
@@ -13,9 +16,16 @@ public class IndexController {
     @Autowired
     private TestService testService;
 
-    @RequestMapping("/")
-    public String index(Model model) {
+    @GetMapping("/")
+    public String index(@ModelAttribute ContentsForm contentsForm, Model model) {
             model.addAttribute("testList", testService.getAll()); 
         return "index";
     }
+
+    @PostMapping("/add-contents")
+    public String addContents(@ModelAttribute ContentsForm contentsForm, Model model) {
+        testService.addContents(contentsForm);
+        return "redirect:/";
+    }
+    
 }
--- src/main/java/com/example/testproject/service/TestService.java
+++ src/main/java/com/example/testproject/service/TestService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.testproject.Entity.TestEntity;
+import com.example.testproject.dto.ContentsForm;
 import com.example.testproject.repository.TestRepository;
 
 @Service
@@ -16,6 +17,20 @@ public class TestService {
 
     public List<TestEntity> getAll(){
         return testRepository.findAll();
+    }
+
+    public void addContents(ContentsForm contentsForm) {
+        TestEntity testEntity = formToEntity(contentsForm);
+		testRepository.save(testEntity);
+	}
+
+    private TestEntity formToEntity(ContentsForm contentsForm) {
+        TestEntity testEntity = new TestEntity();
+        testEntity.setContent1(contentsForm.getContent1());
+        testEntity.setContent2(contentsForm.getContent2());
+        testEntity.setContent3(contentsForm.getContent3());
+
+        return testEntity;
     }
 
 }
